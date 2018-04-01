404fddac3c0998b26872e0acd15bb9b6fd725efb
bartolomeo.sorrentino@gmail.com <bartolomeo.sorrentino@gmail.com>
2018-04-01 11:40:33 +0200

    update webapp

--- webapp/src/pages/home/home.ts
+++ webapp/src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+type Package = {
+  name:string;
+  types:Array<{name:string, export?:boolean, functiona?:boolean}>;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  packages:Array<Package> = [
+    {
+      name:"java.util",
+      types:[
+        {name:"Iterator"},
+        {name:"List"},
+        {name:"Set"},
+        {name:"Map"},
+        {name:"Stack"},
+        {name:"Arrays", export:true},
+        {name:"Base64"},
+        {name:"Collections", export:true},
+        {name:"Optional"}, 
+      ],
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  generate() {
+
+    this.packages.forEach( p => {
+      p.types.forEach( t => console.dir(t))
+    })
   }
 
 }
